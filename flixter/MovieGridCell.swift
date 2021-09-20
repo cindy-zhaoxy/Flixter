b059b5ea62fdcecb746cd6f2a506308c3ed85619
Cindy <59320097+cindy-zhaoxy@users.noreply.github.com>
2021-09-20 14:30:12 -0400

    Flixter Part 2

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Cindy Zhao on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
